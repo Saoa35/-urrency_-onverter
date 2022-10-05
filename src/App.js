6c2351913541b8688e39fc1e0557f3a9ba36f93b
Saoa35 <saoamah@gmail.com>
2022-10-05 21:22:31 +0200

    added Block compon.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import './index.scss';
 
 function App() {
   return (
     <div className="App">
-      <Block value={0} currency="RUB" />
-      <Block value={0} currency="USD" />
+      {/* <Block value={0} currency="RUB" />
+      <Block value={0} currency="USD" /> */}
     </div>
   );
 }
